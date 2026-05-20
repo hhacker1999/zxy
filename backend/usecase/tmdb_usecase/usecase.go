c80afdf0e84256a22c94bb9870dcea742932fe5c
Harsh Kumar <harsh.sadh313@gmail.com>
2026-05-20 22:08:54 +0530

    temp log 2

--- backend/usecase/tmdb_usecase/usecase.go
+++ backend/usecase/tmdb_usecase/usecase.go
@@ -807,7 +807,10 @@ func (u *Usecase) getTMDBMovieGenre() ([]models.Genre, error) {
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println("Error reading get movie genre ", err, res.StatusCode)
+		fmt.Println("--------------------------------------------------")
+		fmt.Println(res.StatusCode)
+		fmt.Println("--------------------------------------------------")
+		fmt.Println("Error reading get movie genre", err)
 		return nil, apperrors.SomethingWentWrongError{}
 	}
 
